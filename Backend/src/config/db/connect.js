4a3cb627696bc2c1e5d1c86379078b2e28f0a90a
HoangNguyen005 <nguyentronghoang205@gmail.com>
2025-04-03 10:04:36 +0700

    fix

--- Backend/src/config/db/connect.js
+++ Backend/src/config/db/connect.js
@@ -3,8 +3,9 @@ const connect = async () => {
     try {
         // mongodb://127.0.0.1/journey_store
         await mongoose.connect(
-            'mongodb+srv://admin:admin@yourney.1n00x7p.mongodb.net/'
+            "mongodb+srv://admin:admin@cluster.mongodb.net/yourney?retryWrites=true&w=majority"
         );
+        // 'mongodb+srv://admin:admin@yourney.1n00x7p.mongodb.net/'
         console.log('Connected to MongoDB successfully');
     } catch (error) {
         console.log('Connected failed');
