38066b16bb41539d39b901e635e9534cc6a710ad
HoangNguyen005 <163545916+HoangNguyen005@users.noreply.github.com>
2025-04-03 22:32:12 +0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,13 +18,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: 'GET,POST,PUT,PATCH,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }));
