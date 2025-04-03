9a69df0a4668998c3f62c6d0cdea575cff2575e6
HoangNguyen005 <nguyentronghoang205@gmail.com>
2025-04-03 22:58:48 +0700

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -30,6 +30,23 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization'
 }));
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  }
+
+  // Xử lý preflight request
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+  }
+
+  next();
+});
+
 // Middleware to parse JSON request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
